341af17f85f3b5f5d7f26e8904d7c7bcdbbbe1c4
Kirill Osenkov <github@osenkov.com>
2016-05-04 18:38:03 -0700

    Add a unit-test project.

---
+++ src/StructuredLogger.Tests/Roundtrip.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Xunit;
+
+namespace StructuredLogger.Tests
+{
+    public class Roundtrip
+    {
+        // [Fact]
+        public void RoundtripTest()
+        {
+            var file = @"D:\log1.xml";
+            var build = LogReader.ReadLog(file);
+            var newName = Path.ChangeExtension(file, ".new.xml");
+            build.SaveToXml(newName);
+            Process.Start("devenv", $"/diff \"{file}\" \"{newName}\"");
+        }
+    }
+}
