5569edccfd1e6bdb2ed410605801a95df3d5ccbe
Kirill Osenkov <github@osenkov.com>
2016-05-16 17:26:46 -0700

    Update version to 1.0.1.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
