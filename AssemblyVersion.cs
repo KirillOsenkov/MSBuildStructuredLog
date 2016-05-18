ddfe37336834f50d31cf2137b259ea64dc49eb1d
Kirill Osenkov <github@osenkov.com>
2016-05-18 11:38:37 -0700

    Bump version to 1.0.3.

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,3 +1,3 @@
 using System.Reflection;
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
