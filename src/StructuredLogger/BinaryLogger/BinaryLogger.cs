a9004b3a4014bdaa3984d393252cad14ed50b246
Kirill Osenkov <github@osenkov.com>
2017-02-23 12:02:56 -0800

    Remove a dependency on Utilities.Core

--- src/StructuredLogger/BinaryLogger/BinaryLogger.cs
+++ src/StructuredLogger/BinaryLogger/BinaryLogger.cs
@@ -2,11 +2,10 @@
 using System.IO;
 using System.IO.Compression;
 using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 
 namespace Microsoft.Build.Logging
 {
-    public class BinaryLogger : Logger
+    public class BinaryLogger : ILogger
     {
         public const int FileFormatVersion = 1;
 
@@ -16,10 +15,12 @@ namespace Microsoft.Build.Logging
 
         public string FilePath { get; set; }
 
-        public override void Initialize(IEventSource eventSource)
+        public LoggerVerbosity Verbosity { get; set; } = LoggerVerbosity.Diagnostic;
+        public string Parameters { get; set; }
+
+        public void Initialize(IEventSource eventSource)
         {
             Environment.SetEnvironmentVariable("MSBUILDTARGETOUTPUTLOGGING", "true");
-            Verbosity = LoggerVerbosity.Diagnostic;
 
             ProcessParameters();
 
@@ -41,9 +42,8 @@ namespace Microsoft.Build.Logging
             eventSource.AnyEventRaised += EventSource_AnyEventRaised;
         }
 
-        public override void Shutdown()
+        public void Shutdown()
         {
-            base.Shutdown();
             if (stream != null)
             {
                 // It's hard to determine whether we're at the end of decoding GZipStream
