988df6abe113c2e64edd2182365ab2e8030cb9da
David Gardiner <david@gardiner.net.au>
2017-09-14 13:56:43 +0930

    Copy files from https://github.com/flcdrg/au-packages/tree/master/msbuild-structured-log-viewer

---
+++ src/StructuredLogViewer/tools/chocolateyuninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'MSBuild Structured Log Viewer'  #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  fileType      = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)
+  silentArgs   = '--uninstall -s'           # Squirrel
+  validExitCodes= @(0) #please insert other valid exit codes here
+}
+
+$uninstall = Get-UninstallRegistryKey -softwareName $packageArgs.softwareName
+
+$packageArgs.file = ($uninstall.InstallLocation + "\update.exe")
+
+Uninstall-ChocolateyPackage @packageArgs
