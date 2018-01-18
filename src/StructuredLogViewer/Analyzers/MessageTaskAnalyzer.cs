72d7b718e089cb7d196bd9ce0c255813f3003b27
Kirill Osenkov <github@osenkov.com>
2018-01-17 20:16:20 -0800

    Counter-intuitively, SingleOrDefault() throws if the sequence contains more than one element. Fix that.

--- src/StructuredLogViewer/Analyzers/MessageTaskAnalyzer.cs
+++ src/StructuredLogViewer/Analyzers/MessageTaskAnalyzer.cs
@@ -6,7 +6,7 @@ namespace Microsoft.Build.Logging.StructuredLogger
     {
         public static void Analyze(Task task)
         {
-            var message = task.Children.OfType<Message>().SingleOrDefault();
+            var message = task.Children.OfType<Message>().FirstOrDefault();
             if (message != null && message.ShortenedText != null)
             {
                 task.Title = "Message: " + message.ShortenedText;
