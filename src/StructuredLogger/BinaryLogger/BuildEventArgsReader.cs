1444322c15b12de23ae775954aa0a5ef1ac919a6
Kirill Osenkov <github@osenkov.com>
2018-08-17 17:47:39 -0700

    Extract the Taskitem implementation to a separate reusable file.

--- src/StructuredLogger/BinaryLogger/BuildEventArgsReader.cs
+++ src/StructuredLogger/BinaryLogger/BuildEventArgsReader.cs
@@ -696,41 +696,6 @@ namespace Microsoft.Build.Logging
             return result;
         }
 
-        private class TaskItem : ITaskItem
-        {
-            public string ItemSpec { get; set; }
-            public Dictionary<string, string> Metadata { get; } = new Dictionary<string, string>();
-
-            public int MetadataCount => Metadata.Count;
-
-            public ICollection MetadataNames => Metadata.Keys;
-
-            public IDictionary CloneCustomMetadata()
-            {
-                return Metadata;
-            }
-
-            public void CopyMetadataTo(ITaskItem destinationItem)
-            {
-                throw new NotImplementedException();
-            }
-
-            public string GetMetadata(string metadataName)
-            {
-                return Metadata[metadataName];
-            }
-
-            public void RemoveMetadata(string metadataName)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void SetMetadata(string metadataName, string metadataValue)
-            {
-                throw new NotImplementedException();
-            }
-        }
-
         private ITaskItem ReadItem()
         {
             var item = new TaskItem();
---
+++ src/StructuredLogger/ObjectModel/TaskItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+
+namespace Microsoft.Build.Logging
+{
+    public class TaskItem : ITaskItem2
+    {
+        public string ItemSpec { get; set; }
+        public Dictionary<string, string> Metadata { get; } = new Dictionary<string, string>();
+        public string EvaluatedIncludeEscaped { get; set; }
+
+        public int MetadataCount => Metadata.Count;
+        public ICollection MetadataNames => Metadata.Keys;
+
+        public IDictionary CloneCustomMetadata()
+        {
+            return Metadata;
+        }
+
+        public IDictionary CloneCustomMetadataEscaped()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyMetadataTo(ITaskItem destinationItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMetadata(string metadataName)
+        {
+            Metadata.TryGetValue(metadataName, out var result);
+            return result;
+        }
+
+        public string GetMetadataValueEscaped(string metadataName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveMetadata(string metadataName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMetadata(string metadataName, string metadataValue)
+        {
+            Metadata[metadataName] = metadataValue;
+        }
+
+        public void SetMetadataValueLiteral(string metadataName, string metadataValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
