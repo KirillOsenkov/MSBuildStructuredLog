2c90a31cb6164bc758be288c099fd87c08ddb0b6
Kirill Osenkov <github@osenkov.com>
2018-10-12 19:32:59 -0700

    Move WPF-independent code to StructuredLogViewer.Core.

---
+++ src/StructuredLogViewer.Core/DialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StructuredLogViewer
+{
+    public static class DialogService
+    {
+        public static void ShowMessageBox(string message)
+        {
+            ShowMessageBoxEvent?.Invoke(message);
+        }
+
+        public static event Action<string> ShowMessageBoxEvent;
+    }
+}
