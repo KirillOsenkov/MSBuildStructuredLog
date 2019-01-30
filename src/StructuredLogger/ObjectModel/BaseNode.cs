6e1be9377d12f0d82798dcadf2e4a24c88f55ca3
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2019-01-30 21:56:52 +0100

    Pack node attributes into a flags enum

--- src/StructuredLogger/ObjectModel/BaseNode.cs
+++ src/StructuredLogger/ObjectModel/BaseNode.cs
@@ -2,6 +2,8 @@
 {
     public abstract class BaseNode : ObservableObject
     {
+        private protected NodeFlags Flags { get; private set; }
+
         /// <summary>
         /// Since there can only be 1 selected node at a time, don't waste an instance field
         /// just to store a bit. Store the currently selected node here and this way we save
@@ -29,6 +31,20 @@
 
                 RaisePropertyChanged();
                 RaisePropertyChanged("IsLowRelevance");
+            }
+        }
+
+        private protected bool HasFlag(NodeFlags flag) => (Flags & flag) == flag;
+
+        private protected void SetFlag(NodeFlags flag, bool value)
+        {
+            if (value)
+            {
+                Flags = Flags | flag;
+            }
+            else
+            {
+                Flags = Flags & ~flag;
             }
         }
     }
---
+++ src/StructuredLogger/ObjectModel/NodeFlags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Build.Logging.StructuredLogger
+{
+    [Flags]
+    internal enum NodeFlags : byte
+    {
+        None = 0,
+        Hidden = 1 << 0,
+        Expanded = 1 << 1
+    }
+}
