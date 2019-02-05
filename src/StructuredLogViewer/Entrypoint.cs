a83de5c28e960d3a1e8f4579a2f3a68180e25bc3
Kirill Osenkov <github@osenkov.com>
2019-02-05 10:10:00 -0800

    Prevent exceptions on UI thread from crashing the app.

--- src/StructuredLogViewer/Entrypoint.cs
+++ src/StructuredLogViewer/Entrypoint.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Threading;
+using Microsoft.Build.Logging.StructuredLogger;
 
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("MSBuild Structured Log Viewer")]
@@ -18,8 +20,17 @@ namespace StructuredLogViewer
             ClipboardService.Set += Clipboard.SetText;
 
             var app = new Application();
+            app.DispatcherUnhandledException += OnDispatcherUnhandledException;
             var window = new MainWindow();
             app.Run(window);
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            ErrorReporting.ReportException(e.Exception);
+            DialogService.ShowMessageBox(
+                    "Unexpected exception. Sorry about that.\r\nPlease Ctrl+C to copy this text and file an issue at https://github.com/KirillOsenkov/MSBuildStructuredLog/issues/new\r\n\r\n" + e.Exception.ToString());
+            e.Handled = true;
         }
     }
 }
