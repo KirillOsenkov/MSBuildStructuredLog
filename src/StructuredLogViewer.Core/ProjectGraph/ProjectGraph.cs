f93b001c9c29bbb087aa93934c545ec937405be8
Mihai Codoban <micodoba@microsoft.com>
2020-02-07 12:27:08 -0800

    Stub out UI

---
+++ src/StructuredLogViewer.Core/ProjectGraph/ProjectGraph.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------
+// 
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+// --------------------------------------------------------------------
+
+using System;
+using Microsoft.Build.Logging.StructuredLogger;
+
+namespace StructuredLogViewer.Core.ProjectGraph
+{
+    public class ProjectGraph
+    {
+        public ProjectGraph(Build build)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
