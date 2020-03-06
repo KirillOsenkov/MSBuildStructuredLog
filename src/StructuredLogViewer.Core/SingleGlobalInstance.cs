6a9a868e65e6536e93b61947da276edd299cb926
Mihai Codoban <micodoba@microsoft.com>
2020-03-06 15:29:50 -0800

    Acquire global lock before writing to settings files
    This allows opening multiple app instances in parallel, without them
    racing on the common settings files.

---
+++ src/StructuredLogViewer.Core/SingleGlobalInstance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace StructuredLogViewer.Core
+{
+    public class SingleGlobalInstance : IDisposable
+    {
+        public bool HasHandle = false;
+        private Mutex mutex;
+
+        public static SingleGlobalInstance Acquire(string mutexName, int millisecondsTimeout = -1)
+        {
+            return new SingleGlobalInstance(mutexName, millisecondsTimeout);
+        }
+
+        private SingleGlobalInstance(string mutexName, int millisecondsTimeout = -1)
+        {
+            try
+            {
+                mutex = new Mutex(false, mutexName);
+                HasHandle = mutex.WaitOne(millisecondsTimeout);
+            }
+            catch (AbandonedMutexException)
+            {
+                HasHandle = true;
+            }
+        }
+ 
+        public void Dispose()
+        {
+            if (mutex != null)
+            {
+                if (HasHandle)
+                {
+                    mutex.ReleaseMutex();
+                }
+ 
+                mutex.Dispose();
+                mutex = null;
+            }
+        }
+    }
+}
