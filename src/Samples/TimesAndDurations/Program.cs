fbe61fe7c3ff223fb424b3026324409d6c197011
Kirill Osenkov <github@osenkov.com>
2020-06-29 18:19:59 -0700

    Add a sample of reading the binlog and displaying target start/end time and duration.

---
+++ src/Samples/TimesAndDurations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Logging.StructuredLogger;
+
+namespace TimesAndDurations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                return;
+            }
+
+            var logFilePath = args[0];
+            if (!File.Exists(logFilePath))
+            {
+                return;
+            }
+
+            var build = Serialization.Read(logFilePath);
+
+            var targets = build
+                .FindChildrenRecursive<Target>(t => t.StartTime > DateTime.MinValue && t.EndTime < DateTime.MaxValue)
+                .OrderByDescending(t => t.Duration)
+                .ToArray();
+
+            foreach (var target in targets.Take(10))
+            {
+                Console.WriteLine($"Target {target.Name}: {TextUtilities.Display(target.StartTime, displayDate: false)} {TextUtilities.Display(target.EndTime, displayDate: false)} {target.DurationText}");
+            }
+        }
+    }
+}
