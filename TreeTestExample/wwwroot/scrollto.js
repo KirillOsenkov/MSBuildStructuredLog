9a0b0dd50cbc58e34f79fdef47dc40a9be3fa48d
Lauren Prinn <t-laprin@microsoft.com>
2020-07-23 17:08:18 -0400

    scrolling fixed

--- TreeTestExample/wwwroot/scrollto.js
+++ TreeTestExample/wwwroot/scrollto.js
@@ -3,7 +3,13 @@
         var element = document.getElementById(elementId);
 
         if (element) {
-            element.scrollIntoView({
+            var tree = document.getElementById("five");
+            if (!tree) {
+                tree = document.getElementById("two");
+            }
+            var pos = element.offsetTop - 15;
+            tree.scroll({
+                top: pos,
                 behavior: 'smooth'
             });
         }
