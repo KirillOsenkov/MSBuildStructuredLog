3f0f59338df03d41abdef7a6b26548620e12448d
Kirill Osenkov <fork@osenkov.com>
2020-07-26 13:56:14 -0700

    Simplify reading resources.

--- src/StructuredLogger/Strings/ResourcesCollection.cs
+++ src/StructuredLogger/Strings/ResourcesCollection.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.Serialization.Json;
+using ResourcesDictionary = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, string>>;
 
 namespace Microsoft.Build.Logging.StructuredLogger
 {
-    public class ResourcesCollection
-    {
-        public Dictionary<string, Dictionary<string, string>> CultureResources { get; set; }
-    }
-
     public class StringsSet
     {
         private static Dictionary<string, string> CurrentSet;
-        private static ResourcesCollection ResourcesCollection;
+        private static ResourcesDictionary ResourcesCollection;
 
         public void InitializeCollection(CultureInfo cultureInfo)
         {
@@ -23,17 +19,17 @@ namespace Microsoft.Build.Logging.StructuredLogger
                 var stream = assembly.GetManifestResourceStream(@"Strings.json");
 
                 var settings = new DataContractJsonSerializerSettings() { UseSimpleDictionaryFormat = true };
-                var d = new DataContractJsonSerializer(typeof(ResourcesCollection), settings);
-                ResourcesCollection = (ResourcesCollection)d.ReadObject(stream);
+                var d = new DataContractJsonSerializer(typeof(ResourcesDictionary), settings);
+                ResourcesCollection = (ResourcesDictionary)d.ReadObject(stream);
             }
 
-            if (ResourcesCollection.CultureResources.ContainsKey(cultureInfo.Name))
+            if (ResourcesCollection.ContainsKey(cultureInfo.Name))
             {
-                CurrentSet = ResourcesCollection.CultureResources[cultureInfo.Name];
+                CurrentSet = ResourcesCollection[cultureInfo.Name];
             }
             else
             {
-                CurrentSet = ResourcesCollection.CultureResources["en-US"];
+                CurrentSet = ResourcesCollection["en-US"];
             }
         }
 
