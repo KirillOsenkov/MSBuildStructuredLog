953d5f3d568654d4af082e18bf2269cdbf18ff79
Lauren Prinn <t-laprin@microsoft.com>
2020-07-27 12:07:15 -0400

    Fixed reloading error causing pages to reset

--- TreeTestExample/wwwroot/scrollto.js
+++ TreeTestExample/wwwroot/scrollto.js
@@ -3,15 +3,20 @@
         var element = document.getElementById(elementId);
 
         if (element) {
-            var tree = document.getElementById("five");
-            if (!tree) {
-                tree = document.getElementById("two");
-            }
+            var tree = document.getElementById("two");
             var pos = element.offsetTop - 15;
             tree.scroll({
                 top: pos,
                 behavior: 'smooth'
             });
         }
+    },
+
+    Split: (splitElem) => {
+        instance = Split(splitElem);
+    },
+
+    Destroy: () => {
+        instance.destroy();
     }
 };
