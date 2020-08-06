b10327a064f6329ac2965a9d764088640d4fb761
Lauren Prinn <t-laprin@microsoft.com>
2020-08-06 15:44:47 -0400

    syntax highlighting for the source files and line highlighting for Targets/Tasks

---
+++ TreeTestExample/Languages.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StructuredLogViewerWASM
+{
+    public class Languages
+    {
+        public static string LanguagePicker(string fileName)
+        {
+            string[] fileParts = fileName.Split(".");
+            string fileExtension = fileParts[fileParts.Length - 1];
+            string language = null;
+            switch (fileExtension)
+            {
+                
+            }
+            return "";
+        }
+    }
+    public enum LanguagesEnum
+    {
+        TypeScript,
+        JavaScript,
+        CSS,
+        LESS,
+        SCSS,
+        JSON,
+        HTML,
+        XML,
+        PHP,
+        csharp,
+        cpp,
+        Razor,
+        Markdown,
+        Diff,
+        Java,
+        VB,
+        CoffeeScript,
+        Handlebars,
+        Batch,
+        Pug,
+        fsharp,
+        Lua,
+        Powershell,
+        Python,
+        Ruby,
+        SASS,
+        R,
+    }
+}
--- TreeTestExample/SourceFileHelper.cs
+++ TreeTestExample/SourceFileHelper.cs
@@ -2,6 +2,8 @@
 using Microsoft.Build.Logging.StructuredLogger;
 using StructuredLogViewer;
 using System.ComponentModel;
+using Microsoft.Language.Xml;
+using System.Linq;
 
 namespace StructuredLogViewerWASM
 {
@@ -14,7 +16,7 @@ namespace StructuredLogViewerWASM
         /// <param name="bn">BaseNode to be reading file from</param>
         public static object[] SourceFileText(ISourceFileResolver fileResolver, BaseNode bn)
         {
-            string path;
+            string path = "";
             string sourceFileText = null;
             string sourceFileName = "";
             int sourceFileLineNumber = -1;
@@ -34,6 +36,7 @@ namespace StructuredLogViewerWASM
                     sourceFileName = ad.Name;
                 }
                 sourceFileLineNumber = ad.LineNumber;
+
             }
             else if (bn is Project)
             {
@@ -46,12 +49,14 @@ namespace StructuredLogViewerWASM
                 path = ((Target)bn).SourceFilePath;
                 sourceFileName = ((Target)bn).Name;
                 sourceFileText = fileResolver.GetSourceFileText(path).Text;
+                sourceFileLineNumber = TargetLineNumber(fileResolver.GetSourceFileText(path), sourceFileName);
             }
             else if (bn is Microsoft.Build.Logging.StructuredLogger.Task)
             {
                 path = ((Microsoft.Build.Logging.StructuredLogger.Task)bn).SourceFilePath;
                 sourceFileName = ((Microsoft.Build.Logging.StructuredLogger.Task)bn).Name;
                 sourceFileText = fileResolver.GetSourceFileText(path).Text;
+                sourceFileLineNumber = TaskLineNumber(fileResolver.GetSourceFileText(path), ((Microsoft.Build.Logging.StructuredLogger.Task)bn).Parent, sourceFileName);
             }
             else if (bn is IHasSourceFile && ((IHasSourceFile)bn).SourceFilePath != null)
             {
@@ -82,11 +87,88 @@ namespace StructuredLogViewerWASM
             {
                 sourceFileText = "No file to display";
             }
-            object[] sourceFileResults = new object[3];
+
+            string[] fileParts = path.Split(".");
+            string fileExtension = fileParts[fileParts.Length - 1];
+            if (fileExtension.Equals("csproj") || fileExtension.Equals("metaproj"))
+            {
+                fileExtension = "xml";
+            }
+            object[] sourceFileResults = new object[4];
             sourceFileResults[0] = sourceFileName;
             sourceFileResults[1] = sourceFileText;
             sourceFileResults[2] = sourceFileLineNumber;
+            sourceFileResults[3] = fileExtension;
             return sourceFileResults;
+        }
+
+        /// <summary>
+        /// Finds the line number for a Task
+        /// </summary>
+        /// <param name="text"> The file information for the target node </param>
+        /// <param name="parent"> Target the task should reside in </param>
+        /// <param name="name"> Name of the task to highlight </param>
+        /// <returns> Line number to highlight</returns>
+        public static int  TaskLineNumber(SourceText text, TreeNode parent, string name)
+        {
+            Target target = parent as Target;
+            if (target == null)
+            {
+                return -1;
+            }
+            return TargetLineNumber(text, target.Name, name);
+        }
+
+        /// <summary>
+        /// Finds the line number for a Target
+        /// </summary>
+        /// <param name="text"> The file information for the target node </param>
+        /// <param name="targetName"> Name of the target to find in the file </param>
+        /// <param name="taskName"> Name of the task to find in the file </param>
+        /// <returns> Line number to highlight</returns>
+        public static int TargetLineNumber(SourceText text, string targetName, string taskName = null)
+        {
+            var xml = text.XmlRoot;
+            IXmlElement root = xml.Root;
+            int startPosition = 0;
+            int line = 0;
+
+            // work around a bug in Xml Parser where a virtual parent is created around the root element
+            // when the root element is preceded by trivia (comment)
+            if (root.Name == null && root.Elements.FirstOrDefault() is IXmlElement firstElement && firstElement.Name == "Project")
+            {
+                root = firstElement;
+            }
+
+            foreach (var element in root.Elements)
+            {
+                if (element.Name == "Target" && element.Attributes != null)
+                {
+                    var nameAttribute = element.AsSyntaxElement.Attributes.FirstOrDefault(a => a.Name == "Name" && a.Value == targetName);
+                    if (nameAttribute != null)
+                    {
+                        startPosition = nameAttribute.ValueNode.Start;
+
+                        if (taskName != null)
+                        {
+                            var tasks = element.Elements.Where(e => e.Name == taskName).ToArray();
+                            if (tasks.Length == 1)
+                            {
+                                startPosition = tasks[0].AsSyntaxElement.NameNode.Start;
+                            }
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            if (startPosition > 0)
+            {
+                line = text.GetLineNumberFromPosition(startPosition);
+            }
+
+            return  line + 1;
         }
     }
 }
