5aa91bb5cb36352735e07260372f343bc2d66777
Lauren Prinn <t-laprin@microsoft.com>
2020-08-12 15:30:56 -0400

    Dark Mode

--- Blazor App/wwwroot/scrollto.js
+++ Blazor App/wwwroot/scrollto.js
@@ -18,5 +18,27 @@
 
     Destroy: () => {
         instance.destroy();
+    },
+
+    DarkMode: () => {
+        var app = document.getElementById("app");
+        app.style.backgroundColor = "#202020";
+        app.style.color = "#E0E0E0"
+        var main = document.getElementById("mainLayout");
+        main.style.backgroundColor = "#202020";
+        main.style.color = "#E0E0E0"
+        var tab = document.getElementById("ui-tabpanel-0");
+        tab.style.backgroundColor = "#202020";
+    },
+
+    LightMode: () => {
+        var app = document.getElementById("app");
+        app.style.backgroundColor = "white";
+        app.style.color = "black"
+        var main = document.getElementById("mainLayout");
+        main.style.backgroundColor = "white";
+        main.style.color = "black"
+        var tab = document.getElementById("ui-tabpanel-0");
+        tab.style.backgroundColor = "white";
     }
 };
