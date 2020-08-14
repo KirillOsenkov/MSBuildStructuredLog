8a3362d8ee5115da0ca54f22718382ae7a6e0dbe
Lauren Prinn <t-laprin@microsoft.com>
2020-08-14 14:28:39 -0400

    Prevent dark mode error

--- Blazor App/wwwroot/scrollto.js
+++ Blazor App/wwwroot/scrollto.js
@@ -22,23 +22,53 @@
 
     DarkMode: () => {
         var app = document.getElementById("app");
-        app.style.backgroundColor = "#202020";
-        app.style.color = "#E0E0E0"
+        if (app)
+        {
+            app.style.backgroundColor = "#202020";
+            app.style.color = "#E0E0E0"
+        }
+        
         var main = document.getElementById("mainLayout");
-        main.style.backgroundColor = "#202020";
-        main.style.color = "#E0E0E0"
+        if (main)
+        {
+            main.style.backgroundColor = "#202020";
+            main.style.color = "#E0E0E0"
+        }
+
         var tab = document.getElementById("ui-tabpanel-0");
+        if (!tab)
+        {
+            tab = document.getElementById("ui-tabpanel-1");
+        }
+        if (!tab)
+        {
+            tab = document.getElementById("ui-tabpanel-2");
+        }
         tab.style.backgroundColor = "#202020";
     },
 
     LightMode: () => {
         var app = document.getElementById("app");
-        app.style.backgroundColor = "white";
-        app.style.color = "black"
+        if (app)
+        {
+            app.style.backgroundColor = "white";
+            app.style.color = "black"
+        }
+
         var main = document.getElementById("mainLayout");
-        main.style.backgroundColor = "white";
-        main.style.color = "black"
+        if (main)
+        {
+            main.style.backgroundColor = "white";
+            main.style.color = "black"
+        }
+
         var tab = document.getElementById("ui-tabpanel-0");
+        if (!tab) {
+            tab = document.getElementById("ui-tabpanel-1");
+        }
+        if (!tab) {
+            tab = document.getElementById("ui-tabpanel-2");
+        }
         tab.style.backgroundColor = "white";
     }
 };
