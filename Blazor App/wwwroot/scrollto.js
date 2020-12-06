2255ea40819a9691ec9eab096646bf561fe695ca
Marius Ungureanu <maungu@microsoft.com>
2020-12-06 04:28:05 +0200

    Migrate theming code to use a simple cascading value to simplify the implementation

--- Blazor App/wwwroot/scrollto.js
+++ Blazor App/wwwroot/scrollto.js
@@ -18,57 +18,5 @@
 
     Destroy: () => {
         instance.destroy();
-    },
-
-    DarkMode: () => {
-        var app = document.getElementById("app");
-        if (app)
-        {
-            app.style.backgroundColor = "#202020";
-            app.style.color = "#E0E0E0"
-        }
-        
-        var main = document.getElementById("mainLayout");
-        if (main)
-        {
-            main.style.backgroundColor = "#202020";
-            main.style.color = "#E0E0E0"
-        }
-
-        var tab = document.getElementById("ui-tabpanel-0");
-        if (!tab)
-        {
-            tab = document.getElementById("ui-tabpanel-1");
-        }
-        if (!tab)
-        {
-            tab = document.getElementById("ui-tabpanel-2");
-        }
-        tab.style.backgroundColor = "#202020";
-    },
-
-    LightMode: () => {
-        var app = document.getElementById("app");
-        if (app)
-        {
-            app.style.backgroundColor = "white";
-            app.style.color = "black"
-        }
-
-        var main = document.getElementById("mainLayout");
-        if (main)
-        {
-            main.style.backgroundColor = "white";
-            main.style.color = "black"
-        }
-
-        var tab = document.getElementById("ui-tabpanel-0");
-        if (!tab) {
-            tab = document.getElementById("ui-tabpanel-1");
-        }
-        if (!tab) {
-            tab = document.getElementById("ui-tabpanel-2");
-        }
-        tab.style.backgroundColor = "white";
     }
 };
