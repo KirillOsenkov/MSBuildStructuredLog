c3b2404ee1903a9b03715ca2e6f2e65340da6239
Kirill Osenkov <github@osenkov.com>
2020-12-18 18:32:51 -0800

    Add a new console tool - BinlogTool.
    Various binlog related command-line utilities.

---
+++ src/BinlogTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinlogTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
