0804f067d30ec00f1a59d68d28ab723fb338de19
Simon Cropp <simon.cropp@gmail.com>
2020-12-31 21:05:20 +1100

    show snapshot testing on a user control

---
+++ src/StructuredLogger.Tests/TextViewerControlTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using StructuredLogViewer.Controls;
+using VerifyTests;
+using Xunit;
+using VerifyXunit;
+
+namespace StructuredLogger.Tests
+{
+    [UsesVerify]
+    public class TextViewerControlTests
+    {
+        static TextViewerControlTests()
+        {
+            VerifyXaml.Enable();
+        }
+
+        [StaFact]
+        public Task Render()
+        {
+            var control = new TextViewerControl();
+            control.SetText("theText");
+            return Verifier.Verify(control);
+        }
+    }
+}
