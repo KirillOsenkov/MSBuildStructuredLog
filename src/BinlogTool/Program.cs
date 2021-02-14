c429f308e781ebd8f62b05978069b4f356d12452
Kirill Osenkov <github@osenkov.com>
2021-02-14 11:32:46 -0800

    Add a way to save strings from a binlog.

--- src/BinlogTool/Program.cs
+++ src/BinlogTool/Program.cs
@@ -13,6 +13,16 @@ namespace BinlogTool
                 var outputRoot = args[2];
 
                 new SaveFiles(args).Run(binlog, outputRoot);
+                return;
+            }
+
+            if (args.Length == 3 && string.Equals(args[0], "savestrings", StringComparison.OrdinalIgnoreCase))
+            {
+                var binlog = args[1];
+                var outputFile = args[2];
+
+                new SaveStrings().Run(binlog, outputFile);
+                return;
             }
 
             Console.WriteLine("Usage: binlogtool savefiles input.binlog");
