KirillOsenkov/MSBuildStructuredLog

commit 90fbd0d01ededc6fe685947bd56fe37b85b8aa6b

Turn off sealing for now to reduce allocations.

src/StructuredLogger/ObjectModel/TreeNode.cs
@@ -79,6 +79,7 @@ namespace Microsoft.Build.Logging.StructuredLogger
             RaisePropertyChanged(nameof(Children));
         }
 
+        [System.Diagnostics.Conditional("TurnedOff")]
         public void Seal()
         {
             if (children != null)

commit 84421e58f83830f11d67d5511ebb563b1f845e38

Fix a weird issue with non-observable children collection
Fixes https://github.com/KirillOsenkov/MSBuildStructuredLog/issues/487

This regressed in https://github.com/KirillOsenkov/MSBuildStructuredLog/commit/90fbd0d01ededc6fe685947bd56fe37b85b8aa6b

src/StructuredLogViewer/Controls/BuildControl.xaml.cs
@@ -1918,7 +1918,17 @@ Recent:
             statsRoot.AddChild(new Property { Name = "UncompressedStreamSize", Value = recordStats.UncompressedStreamSize.ToString("N0") });
             statsRoot.AddChild(new Property { Name = "RecordCount", Value = recordStats.RecordCount.ToString("N0") });
 
-            Build.Seal();
+            // This is interesting. Technically WPF needs the Build.Children collection to be observable
+            // to properly refresh the list when we add a new node. However it suffices to replace the Children
+            // collection with something else (and I assume it gets a new collection view and that is
+            // equivalent to a Reset.
+            // We could literally just do children = children.ToArray() and that would be sufficient here.
+            // Note that there's no need to actually change it to observable collection this late.
+            // Since the children have already mutated by the time we're setting this. Ideally we should be
+            // setting this at the beginning.
+            // It also doesn't seem like raising PropertyChanged for Children is necessary.
+            // See https://github.com/KirillOsenkov/MSBuildStructuredLog/issues/487 for details.
+            Build.MakeChildrenObservable();
         }
 
         private UIElement GetHistogram(List<int> values)

src/StructuredLogger/ObjectModel/TreeNode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 
@@ -86,6 +87,21 @@ namespace Microsoft.Build.Logging.StructuredLogger
             {
                 children = children.ToArray();
             }
+        }
+
+        public void MakeChildrenObservable()
+        {
+            if (children == null)
+            {
+                children = new ObservableCollection<BaseNode>();
+            }
+            else
+            {
+                children = new ObservableCollection<BaseNode>(children);
+            }
+
+            RaisePropertyChanged(nameof(HasChildren));
+            RaisePropertyChanged(nameof(Children));
         }
 
         public void Unseal()