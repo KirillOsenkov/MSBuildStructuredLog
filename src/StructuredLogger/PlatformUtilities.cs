dab398640c5f53a927a6240ed84a9288e1a82f60
Aleksey Kliger <alklig@microsoft.com>
2023-07-21 14:20:10 -0400

    Disable threading on browser
    Fixes https://github.com/KirillOsenkov/MSBuildStructuredLog/issues/690

---
+++ src/StructuredLogger/PlatformUtilities.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace StructuredLogger;
+
+public class PlatformUtilities
+{
+    public static bool HasThreads => !RuntimeInformation.IsOSPlatform(_browser);
+
+    private static readonly OSPlatform _browser = OSPlatform.Create("BROWSER");
+}
