952b5523bab841a6f6f9e94f5d602e98725f634a
Kirill Osenkov <github@osenkov.com>
2023-11-19 13:04:48 -0800

    Remove empty line

--- src/StructuredLogger/PlatformUtilities.cs
+++ src/StructuredLogger/PlatformUtilities.cs
@@ -10,7 +10,6 @@ public class PlatformUtilities
     public static bool HasTempStorage => !_isWasi;
     public static bool HasColor => !_isWasi;
 
-
     private static readonly bool _isBrowser = RuntimeInformation.IsOSPlatform(OSPlatform.Create("BROWSER"));
     private static readonly bool _isWasi = RuntimeInformation.IsOSPlatform(OSPlatform.Create("WASI"));
 }
