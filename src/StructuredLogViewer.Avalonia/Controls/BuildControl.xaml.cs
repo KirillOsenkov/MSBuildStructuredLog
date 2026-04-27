47e3504608a8dc263dd7321dde696a7b6dc28caf
Max Katz <maxkatz6@outlook.com>
2026-04-26 23:38:55 -0700

    Use CompiledBinding instead of reflection

--- src/StructuredLogViewer.Avalonia/Controls/BuildControl.xaml.cs
+++ src/StructuredLogViewer.Avalonia/Controls/BuildControl.xaml.cs
@@ -216,9 +216,12 @@ namespace StructuredLogViewer.Avalonia.Controls
             Style GetTreeViewItemStyle()
             {
                 var treeViewItemStyle = new Style(s => s.OfType<TreeViewItem>());
-                treeViewItemStyle.Setters.Add(new Setter(TreeViewItem.IsExpandedProperty, new Binding("IsExpanded") {Mode = BindingMode.TwoWay}));
-                treeViewItemStyle.Setters.Add(new Setter(TreeViewItem.IsSelectedProperty, new Binding("IsSelected") {Mode = BindingMode.TwoWay}));
-                treeViewItemStyle.Setters.Add(new Setter(IsVisibleProperty, new Binding("IsVisible") {Mode = BindingMode.TwoWay}));
+                treeViewItemStyle.Setters.Add(new Setter(TreeViewItem.IsExpandedProperty,
+                    CompiledBinding.Create<Item, bool>(i => i.IsExpanded, mode: BindingMode.TwoWay)));
+                treeViewItemStyle.Setters.Add(new Setter(TreeViewItem.IsSelectedProperty,
+                    CompiledBinding.Create<Item, bool>(i => i.IsSelected, mode: BindingMode.TwoWay)));
+                treeViewItemStyle.Setters.Add(new Setter(IsVisibleProperty,
+                    CompiledBinding.Create<Item, bool>(i => i.IsVisible, mode: BindingMode.TwoWay)));
                 return treeViewItemStyle;
             }
 
